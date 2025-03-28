b34e97d051e48b080b82637b4e4fa52b54bdbb72
Andrey <andreeey.gg@gmail.com>
2025-03-28 13:57:07 +0100

    EXAM-4: create product entity

---
+++ furniture-server/src/models/product/product.entity.ts
@@ -0,0 +1,43 @@
+import { Entity, Column } from 'typeorm';
+
+import { AbstractEntity } from '../abstract.entity';
+
+
+export enum ProductType {
+  FURNITURE = 'FURNITURE',
+  DECOR = 'DECOR',
+  ACCESSORIES = 'ACCESSORIES',
+}
+
+@Entity('products')
+export class Product extends AbstractEntity{
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 500 })
+  description: string;
+
+  @Column({ type: 'float' })
+  price: number;
+
+  @Column({ type: 'float', nullable: true })
+  discountPrice?: number;
+
+  @Column({ type: 'varchar', length: 20 })
+  currency: string;
+
+  @Column('text', { array: true })
+  images: string[];
+
+  @Column({ type: 'enum', enum: ProductType })
+  type: ProductType;
+
+  @Column('text', { array: true })
+  sizes: string[];
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  color?: string;
+
+  @Column({ default: false })
+  isBestSeller: boolean;
+}
