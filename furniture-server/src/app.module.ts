623075cca9fe3dbc7a21da2ce2ea43093ab2265e
Andrey <andreeey.gg@gmail.com>
2025-03-31 15:27:45 +0200

    EXAM-11: add dataSource, integrate TypeORM with AppModule

--- furniture-server/src/app.module.ts
+++ furniture-server/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { dataSourceOptions } from '../db/data-source';
 
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [],
   providers: [],
 })
