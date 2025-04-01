9b9ec4eba623d4dbc703085f5ee76070dc1b45eb
Andrey <andreeey.gg@gmail.com>
2025-04-01 17:56:58 +0200

    EXAM-12: create-migrations

---
+++ furniture-server/db/data-source.ts
@@ -0,0 +1,24 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+
+dotenv.config();
+const configService = new ConfigService();
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: configService.get<string>('POSTGRES_DB_HOST'),
+    port: Number(configService.get<string>('POSTGRES_DB_PORT')),
+    username: configService.get<string>('POSTGRES_DB_USER'),
+    password: configService.get<string>('POSTGRES_DB_PASSWORD'),
+    database: configService.get<string>('POSTGRES_DB_NAME'),
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/db/migrations/*.js'],
+    synchronize: false,
+    logging: true,
+}
+
+const dataSource = new DataSource(dataSourceOptions)
+
+export default dataSource;
