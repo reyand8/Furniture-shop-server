584a847740ef95e64ad6a51eedeea22a5e074177
Andrey <andreeey.gg@gmail.com>
2025-04-01 13:02:08 +0200

    EXAM-11: add database service and update env variable names

--- furniture-server/db/database.module.ts
+++ furniture-server/db/database.module.ts
@@ -1,18 +1,16 @@
 import { ConfigService, ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { DataSourceOptions } from 'typeorm';
 
-import { databaseConfig } from './database.config';
+import { DatabaseService } from './database.service';
 
 
 @Module({
     imports: [
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService): DataSourceOptions =>
-                databaseConfig(configService),
+            useClass: DatabaseService,
+            inject: [DatabaseService, ConfigService],
         }),
     ],
 })
