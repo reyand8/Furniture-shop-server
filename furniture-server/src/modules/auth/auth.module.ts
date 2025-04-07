21d6fb936f9ecc5910a7b4d8f3f5aea4514ca68b
Andrey <andreeey.gg@gmail.com>
2025-04-07 16:19:31 +0200

    EXAM-14: replace process.env with ConfigService in JwtModule configuration

--- furniture-server/src/modules/auth/auth.module.ts
+++ furniture-server/src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { ContactInfo } from '../../models/contact-info/contact-info.entity';
 import { UserEntity } from '../../models/user/user.entity';
@@ -11,7 +12,14 @@ import { AuthService } from './auth.service';
 @Module({
     imports: [
         TypeOrmModule.forFeature([UserEntity, ContactInfo]),
-        JwtModule.register({ secret: process.env.JWT_SECRET_KEY, signOptions: { expiresIn: '120m' } })
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET_KEY'),
+                signOptions: { expiresIn: '120m' },
+            }),
+            inject: [ConfigService],
+        }),
     ],
     controllers: [AuthController],
     providers: [AuthService],
