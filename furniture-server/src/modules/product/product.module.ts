462d7bbca3405ff671c88cc54708a12346f6e211
Andrey <andreeey.gg@gmail.com>
2025-04-15 10:30:27 +0200

    EXAM-22: remove AuthModule from ProductModule

--- furniture-server/src/modules/product/product.module.ts
+++ furniture-server/src/modules/product/product.module.ts
@@ -3,14 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
-import { AuthModule } from '../auth/auth.module';
 import { CategoryEntity } from '../../models/category/category.entity';
 
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([CategoryEntity]),
-        AuthModule
     ],
     controllers: [ProductController],
     providers: [ProductService],
