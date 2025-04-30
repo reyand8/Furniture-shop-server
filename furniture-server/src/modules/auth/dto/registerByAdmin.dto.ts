13b14a8bcf3045f8ae3e0b75e28c31102e8bcb4e
Andrey <andreeey.gg@gmail.com>
2025-04-30 15:56:48 +0200

    EXAM-31: Remove duplicated fields from registration form

--- furniture-server/src/modules/auth/dto/registerByAdmin.dto.ts
+++ furniture-server/src/modules/auth/dto/registerByAdmin.dto.ts
@@ -1,34 +1,12 @@
 import {
-    IsEmail, IsEnum, IsNotEmpty, IsString,
-    MaxLength, MinLength
+    IsEnum, IsNotEmpty,
 } from 'class-validator';
 
 import { EUserRole } from '../../../models/user/user.entity';
+import { RegisterUserDto } from './registerUser.dto';
 
 
-export class RegisterByAdminDto {
-    @IsString({ message: 'First name must be a string.' })
-    @IsNotEmpty({ message: 'First name is required.' })
-    @MaxLength(60, { message: 'First name can not be longer than 60 characters.' })
-    firstName: string;
-
-    @IsString({ message: 'Last name must be a string.' })
-    @IsNotEmpty({ message: 'Last name is required.' })
-    @MaxLength(60, { message: 'Last name can not be longer than 60 characters.' })
-    lastName: string;
-
-    @IsString({ message: 'Email must be a string.' })
-    @IsEmail({}, { message: 'Please provide a valid email address.' })
-    @IsNotEmpty({ message: 'Email is required.' })
-    @MaxLength(60, { message: 'Email can not be longer than 60 characters.' })
-    email: string;
-
-    @IsString({ message: 'Password must be a string.' })
-    @MinLength(6, { message: 'Password must be at least 6 characters long.' })
-    @MaxLength(30, { message: 'Password can not be longer than 30 characters.' })
-    @IsNotEmpty({ message: 'Password is required.' })
-    password: string;
-
+export class RegisterByAdminDto extends RegisterUserDto {
     @IsEnum(EUserRole, { message: 'Role must be one of USER, ADMIN, or SUPER_ADMIN.' })
     @IsNotEmpty({ message: 'Role is required.' })
     role: EUserRole;
